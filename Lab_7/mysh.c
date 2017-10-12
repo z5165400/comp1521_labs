c2656b3c25d1db3dd7a8ffd38d2f8434a2acf37c
z5165400 <andrew.walls2@gmail.com>
2017-10-13 10:39:15 +1100

    Lab 7

--- Lab_7/mysh.c
+++ Lab_7/mysh.c
@@ -1,6 +1,6 @@
 // mysh.c ... a very simple shell
 // Started by John Shepherd, October 2017
-// Completed by <<YOU>>, October 2017
+// Completed by Andrew Walls (z5165400), October 2017
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,7 +13,7 @@
 #include <unistd.h>
 #include <assert.h>
 
-extern char *strdup(char *);
+//extern char *strdup(char *);
 void trim(char *);
 char **tokenise(char *, char *);
 void freeTokens(char **);
@@ -22,69 +22,110 @@ void execute(char **, char **, char **);
 
 int main(int argc, char *argv[], char *envp[])
 {
-   pid_t pid;   // pid of child process
-   int stat;    // return status of child
-   char **path; // array of directory names
+    pid_t pid;   // pid of child process
+    int stat;    // return status of child
+    char **path; // array of directory names
 
-   // set up command PATH from environment variable
-   int i;
-   for (i = 0; envp[i] != NULL; i++) {
-      if (strncmp(envp[i], "PATH", 4) == 0) break;
-   }
-   if (envp[i] == NULL)
-      path = tokenise("/bin:/usr/bin",":");
-   else
-      // &envp[i][5] ignores "PATH="
-      path = tokenise(&envp[i][5],":");
+    // set up command PATH from environment variable
+    int i;
+    for (i = 0; envp[i] != NULL; i++) {
+        if (strncmp(envp[i], "PATH", 4) == 0) break;
+    }
+    if (envp[i] == NULL)
+        path = tokenise("/bin:/usr/bin",":");
+    else
+        // &envp[i][5] ignores "PATH="
+        path = tokenise(&envp[i][5],":");
 
 #ifdef DBUG
-   for (i = 0; path[i] != NULL;i++)
-      printf("dir[%d] = %s\n",i,path[i]);
+    for (i = 0; path[i] != NULL;i++)
+        printf("dir[%d] = %s\n",i,path[i]);
 #endif
 
-   // main loop: print prompt, read line, execute command
-   char line[BUFSIZ];
-   printf("mysh$ ");
-   while (fgets(line, BUFSIZ, stdin) != NULL) {
-      trim(line); // remove leading/trailing space
-      if (strcmp(line,"exit") == 0) break;
-      if (strcmp(line,"") == 0) { printf("mysh$ "); continue; }
+    // main loop: print prompt, read line, execute command
+    char line[BUFSIZ];
+    printf("mysh$ ");
+    while (fgets(line, BUFSIZ, stdin) != NULL) {
+        trim(line); // remove leading/trailing space
+        if (strcmp(line,"exit") == 0) break;
+        if (strcmp(line,"") == 0) { printf("mysh$ "); continue; }
 
-      // TODO: implement the tokenise/fork/execute/cleanup code
+        char **args = tokenise(line, " ");
+        int fork_stat = fork();
+        // Child process
+        if(fork_stat == 0) {
+            execute(args, path, envp);
+        } else if(fork_stat > 0) {
+            pid = fork_stat;
+            stat = waitpid(pid, NULL, 0);
+        } else {
+            fprintf(stderr, "Error in fork()\n");
+            exit(EXIT_FAILURE);
+        }
+        freeTokens(args);
 
-      printf("mysh$ ");
-   }
-   printf("\n");
-   return(EXIT_SUCCESS);
+        printf("mysh$ ");
+    }
+    printf("\n");
+    return(EXIT_SUCCESS);
 }
 
 // execute: run a program, given command-line args, path and envp
 void execute(char **args, char **path, char **envp)
 {
-   // TODO: implement the find-the-executable and execve() it code
+    char *cmd = NULL;
+
+#ifdef DBUG
+    for (int i = 0; args[i] != NULL;i++)
+        printf("args[%d] = %s\n",i,args[i]);
+#endif
+
+    if((args[0][0] == '/' || args[0][0] == '.') && isExecutable(args[0])) {
+        cmd = args[0];
+    } else {
+        for(int i = 0; path[i] != NULL && cmd == NULL; i++) {
+            char str[BUFSIZ];
+            strlcpy(str, path[i], BUFSIZ);
+            strlcat(str, "/", BUFSIZ);
+            strlcat(str, args[0], BUFSIZ);
+
+            if(isExecutable(str)) {
+                cmd = str;
+            }
+        }
+    }
+
+    if(cmd == NULL) {
+        fprintf(stderr, "Command not found\n");
+    } else {
+        printf("Executing %s\n", cmd);
+        execve(cmd, args, envp);
+        perror("Exec failed");
+    }
+    exit(EXIT_FAILURE);
 }
 
 // isExecutable: check whether this process can execute a file
 int isExecutable(char *cmd)
 {
-   struct stat s;
-   // must be accessible
-   if (stat(cmd, &s) < 0)
-      return 0;
-   // must be a regular file
-   //if (!(s.st_mode & S_IFREG))
-   if (!S_ISREG(s.st_mode))
-      return 0;
-   // if it's owner executable by us, ok
-   if (s.st_uid == getuid() && s.st_mode & S_IXUSR)
-      return 1;
-   // if it's group executable by us, ok
-   if (s.st_gid == getgid() && s.st_mode & S_IXGRP)
-      return 1;
-   // if it's other executable by us, ok
-   if (s.st_mode & S_IXOTH)
-      return 1;
-   return 0;
+    struct stat s;
+    // must be accessible
+    if (stat(cmd, &s) < 0)
+        return 0;
+    // must be a regular file
+    //if (!(s.st_mode & S_IFREG))
+    if (!S_ISREG(s.st_mode))
+        return 0;
+    // if it's owner executable by us, ok
+    if (s.st_uid == getuid() && s.st_mode & S_IXUSR)
+        return 1;
+    // if it's group executable by us, ok
+    if (s.st_gid == getgid() && s.st_mode & S_IXGRP)
+        return 1;
+    // if it's other executable by us, ok
+    if (s.st_mode & S_IXOTH)
+        return 1;
+    return 0;
 }
 
 // tokenise: split a string around a set of separators
@@ -92,46 +133,46 @@ int isExecutable(char *cmd)
 // final array element contains NULL
 char **tokenise(char *str, char *sep)
 {
-   // temp copy of string, because strtok() mangles it
-   char *tmp;
-   // count tokens
-   tmp = strdup(str);
-   int n = 0;
-   strtok(tmp, sep); n++;
-   while (strtok(NULL, sep) != NULL) n++;
-   free(tmp);
-   // allocate array for argv strings
-   char **strings = malloc((n+1)*sizeof(char *));
-   assert(strings != NULL);
-   // now tokenise and fill array
-   tmp = strdup(str);
-   char *next; int i = 0;
-   next = strtok(tmp, sep);
-   strings[i++] = strdup(next);
-   while ((next = strtok(NULL,sep)) != NULL)
-      strings[i++] = strdup(next);
-   strings[i] = NULL;
-   free(tmp);
-   return strings;
+    // temp copy of string, because strtok() mangles it
+    char *tmp;
+    // count tokens
+    tmp = strdup(str);
+    int n = 0;
+    strtok(tmp, sep); n++;
+    while (strtok(NULL, sep) != NULL) n++;
+    free(tmp);
+    // allocate array for argv strings
+    char **strings = malloc((n+1)*sizeof(char *));
+    assert(strings != NULL);
+    // now tokenise and fill array
+    tmp = strdup(str);
+    char *next; int i = 0;
+    next = strtok(tmp, sep);
+    strings[i++] = strdup(next);
+    while ((next = strtok(NULL,sep)) != NULL)
+        strings[i++] = strdup(next);
+    strings[i] = NULL;
+    free(tmp);
+    return strings;
 }
 
 // freeTokens: free memory associated with array of tokens
 void freeTokens(char **toks)
 {
-   for (int i = 0; toks[i] != NULL; i++)
-      free(toks[i]);
-   free(toks);
+    for (int i = 0; toks[i] != NULL; i++)
+        free(toks[i]);
+    free(toks);
 }
 
 // trim: remove leading/trailing spaces from a string
 void trim(char *str)
 {
-   int first, last;
-   first = 0;
-   while (isspace(str[first])) first++;
-   last  = strlen(str)-1;
-   while (isspace(str[last])) last--;
-   int i, j = 0;
-   for (i = first; i <= last; i++) str[j++] = str[i];
-   str[j] = '\0';
+    int first, last;
+    first = 0;
+    while (isspace(str[first])) first++;
+    last  = strlen(str)-1;
+    while (isspace(str[last])) last--;
+    int i, j = 0;
+    for (i = first; i <= last; i++) str[j++] = str[i];
+    str[j] = '\0';
 }
