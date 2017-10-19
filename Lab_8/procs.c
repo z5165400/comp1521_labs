eb04b753c34615ba884e1386bdbdf27fe3a6a79f
z5165400 <andrew.walls2@gmail.com>
2017-10-20 09:41:21 +1100

    Initial Lab 8 commit

---
+++ Lab_8/procs.c
@@ -0,0 +1,41 @@
+// COMP1521 17s2 Lab08 ... processes competing for a resource
+ 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define MAXLINE BUFSIZ
+
+void copyInput(char *);
+
+int main(void)
+{
+   struct sigaction act;
+   memset (&act, 0, sizeof(act));
+
+   if (fork() != 0) {
+      copyInput("Parent");
+   }
+   else if (fork() != 0) {
+      copyInput("Child");
+   }
+   else {
+      copyInput("Grand-child");
+   }
+   return 0;
+}
+
+void copyInput(char *name)
+{
+   pid_t mypid = getpid();
+   char  line[MAXLINE];
+   printf("%s (%d) ready\n", name, mypid);
+   while (fgets(line, MAXLINE, stdin) != NULL) {
+      printf("%s: %s", name, line);
+      sleep(random()%3);
+   }
+   printf("%s quitting\n", name);
+   return;
+}
