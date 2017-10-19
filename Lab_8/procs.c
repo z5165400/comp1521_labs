66838ad1f10a5bb123a63df0529ec52198b5f091
z5165400 <andrew.walls2@gmail.com>
2017-10-20 09:55:41 +1100

    Indentation

--- Lab_8/procs.c
+++ Lab_8/procs.c
@@ -1,5 +1,5 @@
 // COMP1521 17s2 Lab08 ... processes competing for a resource
- 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -12,30 +12,30 @@ void copyInput(char *);
 
 int main(void)
 {
-   struct sigaction act;
-   memset (&act, 0, sizeof(act));
+    struct sigaction act;
+    memset (&act, 0, sizeof(act));
 
-   if (fork() != 0) {
-      copyInput("Parent");
-   }
-   else if (fork() != 0) {
-      copyInput("Child");
-   }
-   else {
-      copyInput("Grand-child");
-   }
-   return 0;
+    if (fork() != 0) {
+        copyInput("Parent");
+    }
+    else if (fork() != 0) {
+        copyInput("Child");
+    }
+    else {
+        copyInput("Grand-child");
+    }
+    return 0;
 }
 
 void copyInput(char *name)
 {
-   pid_t mypid = getpid();
-   char  line[MAXLINE];
-   printf("%s (%d) ready\n", name, mypid);
-   while (fgets(line, MAXLINE, stdin) != NULL) {
-      printf("%s: %s", name, line);
-      sleep(random()%3);
-   }
-   printf("%s quitting\n", name);
-   return;
+    pid_t mypid = getpid();
+    char  line[MAXLINE];
+    printf("%s (%d) ready\n", name, mypid);
+    while (fgets(line, MAXLINE, stdin) != NULL) {
+        printf("%s: %s", name, line);
+        sleep(random()%3);
+    }
+    printf("%s quitting\n", name);
+    return;
 }
